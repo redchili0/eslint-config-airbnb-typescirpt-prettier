8d8e37cb35f6e13ce346bd8586bdaaaa9c3ba5aa
ben14 <544652471@qq.com>
2019-12-26 15:33:47 +0800

    feat: add all file.

---
+++ index.js
@@ -0,0 +1,42 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: [
+    "airbnb",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/typescript",
+    "plugin:prettier/recommended",
+    "prettier/@typescript-eslint",
+    "prettier/react"
+  ],
+  settings: {
+    "import/extensions": [".js", ".jsx", ".ts", ".tsx"],
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"]
+    },
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"]
+      }
+    }
+  },
+  env: {
+    browser: true,
+    es6: true
+  },
+  rules: {
+    "prettier/prettier": ["error"],
+    "react/jsx-filename-extension": ["error", { extensions: [".tsx", ".jsx"] }],
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        mjs: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never"
+      }
+    ]
+  }
+};
---
+++ readme.md
@@ -0,0 +1,19 @@
+## ESLint Setup for a TypeScript project using Airbnb config with Prettier
+
+### Usage
+
+1. install
+```
+yarn add @redchili/eslint-config-airbnb-typescript-prettier --dev
+```
+or
+```
+npm install @redchili/eslint-config-airbnb-typescript-prettier --save-dev
+```
+
+2. setup .eslintrc
+```json
+{
+  "extends": "@redchili/eslint-config-airbnb-typescript-prettier"
+}
+```
