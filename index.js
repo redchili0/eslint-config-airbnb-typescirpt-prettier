5723139340c16922311b5ee59d6c7397837addc6
ben14 <544652471@qq.com>
2019-12-26 17:17:32 +0800

    feat: add some plugin setup.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "prettier"],
+  plugins: ["react", "jsx-a11y", "import", "@typescript-eslint", "prettier"],
   extends: [
     "airbnb",
     "plugin:@typescript-eslint/recommended",
